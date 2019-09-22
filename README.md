e7b0064deddce470d6a66e80d6e056f0009056e4
Jorge Ovalle <jorge.ovalle@wizeline.com>
2019-09-22 18:07:42 -0500

    Fix Readme and tests

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # fraction_arithmetics
 ## Description
-This small command line executes operations between fractions given a string input.
+This small command-line executes operations between fractions given string input.
 
 ## Installation
-As this command line is using the [Swift Package Manager](https://swift.org/package-manager/), once you clone the repo you should build the sources.
+As this command line is using the [Swift Package Manager](https://swift.org/package-manager/), once you clone the repo, you should build the sources.
 
 `swift build`
 
-once is finished you can run the command line from the build directory or from the installed package.
+Once is finished you can run the command line from the build directory or the installed package.
 
 `.build/debug/Fractions_arithmetics <Arguments>`
 
@@ -24,12 +24,12 @@ or
 `swift run Fractions_arithmetics -o "1_1/2 + 1_1/2"`
 
 The valid inputs for operands are:
-* `1`, `-1` whole numbers
+* `1`, `-1` whole number
 * `1_1/4`, `-1_1/4` mixed fractions
 * `2/4`, `-2/4` proper fractions
 * `20/4`, `-20/4` improper fractions
 
-⚠️ This script supports more than two operands but the precedence is not being taking into account. (`"1/3 + 1/3 + 1/3" is still valid).
+⚠️ This script supports more than two operands, but the operators' precedence is not being taken into account. (`"1/3 + 1/3 + 1/3" is still valid).
 
 `-t` or `--test` Run the tests for parser and operations.
 In `sources/Fractions_arithmetics/Tests.swift` you can find the unit tests are being run.
--- Sources/Fractions_arithmetics/Tests.swift
+++ Sources/Fractions_arithmetics/Tests.swift
@@ -2,7 +2,13 @@ import Foundation
 
 struct Tests {
     func run () {
-        
+        runInstanceTests()
+        runSimplifyTests()
+        runCustomStringConvertibleStringTests()
+        runOperationTests()
+    }
+    
+    private func runInstanceTests() {
         print("\n INSTANCE TESTS")
         let instanceTests = [runInstanceTest(try! FractionParser.parse("20"), Fraction(numerator: 20, denominator: 1)),
                              runInstanceTest(try! FractionParser.parse("0"), Fraction(numerator: 0, denominator: 1)),
@@ -13,7 +19,9 @@ struct Tests {
                              runInstanceTest(try! FractionParser.parse("-1_1/2"), Fraction(numerator: -3, denominator: 2))]
         
         instanceTests.forEach { print($0) }
-        
+    }
+    
+    private func runSimplifyTests() {
         print("\n SIMPLIFY TESTS")
         let simplifyTests = [runInstanceTest(try! FractionParser.parse("20/2").simplify(), Fraction(numerator: 10, denominator: 1)),
                              runInstanceTest(try! FractionParser.parse("-6/8").simplify(), Fraction(numerator: -3, denominator: 4)),
@@ -21,27 +29,29 @@ struct Tests {
                              runInstanceTest(try! FractionParser.parse("2_2/8").simplify(), Fraction(numerator: 9, denominator: 4))]
         
         simplifyTests.forEach { print($0) }
+    }
+    
+    private func runCustomStringConvertibleStringTests() {
+        print("\n CUSTOM STRING CONVERTIBLE")
+        let printTests = [runParseTest(try! FractionParser.parse("1").description, "1"),
+                          runParseTest(try! FractionParser.parse("-5").description, "-5"),
+                          runParseTest(try! FractionParser.parse("0").description, "0"),
+                          runParseTest(try! FractionParser.parse("1/2").description, "1/2"),
+                          runParseTest(try! FractionParser.parse("-1/2").description, "-1/2"),
+                          runParseTest(try! FractionParser.parse("0/2").description, "0"),
+                          runParseTest(try! FractionParser.parse("10/5").description, "2"),
+                          runParseTest(try! FractionParser.parse("1_1/2").description, "1_1/2"),
+                          runParseTest(try! FractionParser.parse("-1_1/2").description, "-1_1/2"),
+                          runParseTest(try! FractionParser.parse("0_1/2").description, "1/2"),
+                          runParseTest(try! FractionParser.parse("1_10/2").description, "6"),
+                          runParseTest(try! FractionParser.parse("-1_10/2").description, "-6"),
+                          runParseTest(try! FractionParser.parse("1_0/2").description, "1")]
         
-        
-        print("\n PARSE TESTS")
-        let parseTests = [runParseTest("1", "1"),
-                          runParseTest("-5", "-5"),
-                          runParseTest("0", "0"),
-                          runParseTest("1/2", "1/2"),
-                          runParseTest("-1/2", "-1/2"),
-                          runParseTest("0/2", "0"),
-                          runParseTest("10/2", "5"),
-                          runParseTest("1_1/2", "1_1/2"),
-                          runParseTest("-1_1/2", "-1_1/2"),
-                          runParseTest("0_1/2", "1/2"),
-                          runParseTest("1_10/2", "6"),
-                          runParseTest("-1_10/2", "-6"),
-                          runParseTest("1_0/2", "1")]
-        
-        parseTests.forEach { print($0) }
-        
+        printTests.forEach { print($0) }
+    }
+    
+    private func runOperationTests() {
         print("\n OPERATION TESTS")
-        
         let operationTests = [runOperationTest("1/2 + 1/2", "1"),
                               runOperationTest("-1/2 + 1/2", "0"),
                               runOperationTest("1/2 + -1/2", "0"),
@@ -51,17 +61,14 @@ struct Tests {
                               runOperationTest("1_1/2 + 1_1/2", "3"),
                               runOperationTest("1_1/3 + 1_1/3", "2_2/3"),
                               runOperationTest("1_1/3 + -1_1/3", "0"),
-                              
                               runOperationTest("2_1/3 - -1_1/3", "3_2/3"),
                               runOperationTest("2_1/3 - 1_1/3", "1"),
                               runOperationTest("2 - 1", "1"),
                               runOperationTest("1/3 - 1_1/3", "-1"),
-                              
                               runOperationTest("2_1/3 * -1_1/3", "-3_1/9"),
                               runOperationTest("2_1/3 * 1_1/3", "3_1/9"),
                               runOperationTest("2 * 1", "2"),
                               runOperationTest("1/3 * 1_1/3", "4/9"),
-                              
                               runOperationTest("2_1/3 / -1_1/3", "-1_3/4"),
                               runOperationTest("-2_1/3 / -1_1/3", "1_3/4"),
                               runOperationTest("2_1/3 / 1_1/3", "1_3/4"),
@@ -71,6 +78,9 @@ struct Tests {
         operationTests.forEach { print($0) }
     }
     
+}
+
+extension Tests {
     private func runInstanceTest(_ input: Fraction, _ expectedOutput: Fraction) -> (Bool, String) {
         return assert(input, expectedOutput)
     }
@@ -97,6 +107,4 @@ struct Tests {
             return (false, "❌ \(input) != \(expectedOutput)")
         }
     }
-    
-    
 }
